8732533fd4a81a107ffbb49fd4c4a089d03f5829
Francesco Corso <fcorso2016@gmail.com>
2023-07-30 10:23:33 -0400

    Indented the #ifdefs for readability

--- model/RPG_global.h
+++ model/RPG_global.h
@@ -4,15 +4,15 @@
 #pragma once
 
 #ifdef RPG_SHARED
-#ifdef _WIN32
-#ifdef RPG_BUILD
-#define RPG_API __declspec(dllexport)
+    #ifdef _WIN32
+        #ifdef RPG_BUILD
+            #define RPG_API __declspec(dllexport)
+        #else
+            #define RPG_API __declspec(dllimport)
+        #endif
+    #else
+        #define RPG_API __attribute__((visibility("default")))
+    #endif
 #else
-#define RPG_API __declspec(dllimport)
-#endif
-#else
-#define RPG_API __attribute__((visibility("default")))
-#endif
-#else
-#define RPG_API
+    #define RPG_API
 #endif
